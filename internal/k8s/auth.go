29cafde36c5718b9b0dad70260f3922299cbbf30
João Vitor Costa <joao.costa@wildlifestudios.com>
2021-12-02 15:35:24 -0300

    Use local authentication with kubeconfig as alternative if the API is not running from a K8s pod

--- internal/k8s/auth.go
+++ internal/k8s/auth.go
@@ -1,10 +1,12 @@
 package k8s
 
 import (
+	"fmt"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
+	"os"
 )
 
 type Auth struct {
@@ -15,12 +17,14 @@ type Auth struct {
 func Authenticate() *Auth {
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalf("Could not retrieve pod service Account configuration: %v", err)
+		log.Printf("Could not retrieve pod service Account configuration: %v", err)
+		log.Print("Using local authentication")
+		return LocalAuthenticate()
 	}
 
 	client, err := dynamic.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Could not authenticate to cluster as a pod: %v", err)
+		log.Fatalf("Could not create client as a pod: %v", err)
 	}
 
 	return &Auth{
@@ -30,14 +34,21 @@ func Authenticate() *Auth {
 }
 
 func LocalAuthenticate() *Auth {
-	config, err := clientcmd.BuildConfigFromFlags("", "/Users/joao.costa/.kube/config")
+	var kubeConfigPath string
+	kubeConfigPath = os.Getenv("KUBECONFIG")
+	if kubeConfigPath == "" {
+		kubeConfigPath = fmt.Sprintf("%s/.kube/config", os.Getenv("HOME"))
+		log.Printf("KUBECONFIG env is unset, using default location in $HOME: %s", kubeConfigPath)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
-		log.Fatalf("Could not retrieve pod service Account configuration: %v", err)
+		log.Fatalf("Could not retrieve Kubeconfig configuration: %v", err)
 	}
 
 	client, err := dynamic.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Could not authenticate to cluster as a pod: %v", err)
+		log.Fatalf("Could not create client using Kubeconfig: %v", err)
 	}
 
 	return &Auth{
